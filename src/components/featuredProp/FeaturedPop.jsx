e7189e517ee44d2c9003c1ed2487f0113c06eebe
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-08-26 17:20:58 +0300

    update

--- src/components/featuredProp/FeaturedPop.jsx
+++ src/components/featuredProp/FeaturedPop.jsx
@@ -3,16 +3,10 @@ import "./featuredProp.css"
 const FeaturedPop = () => {
   return (
     <div className="fp">
-<<<<<<< HEAD
-      <div className="fpItem">
-=======
     <div className="fpItem">
->>>>>>> 95dc31c02114263f054b90ae51b000c38ff372e7
+      
     <img
-     src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/179362750.jpg?k=fd442de52ed044b3eb928255e6c0f3a3499c3885616123982384732064625abb&o=&hp=1" 
-     alt=""
-     className="fpImg"
-      />
+     src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/179362750.jpg?k=fd442de52ed044b3eb928255e6c0f3a3499c3885616123982384732064625abb&o=&hp=1" alt=""className="fpImg" />
     <span className="fpName"> Aparthotel Tell Aviv</span>
     <span className="fpCity"> Tell Aviv</span>
     <span className="fpPrice"> Starting from $120</span>
@@ -20,69 +14,16 @@ const FeaturedPop = () => {
     <button>8.9</button>
     <span>Excellent</span>
     </div>
-<<<<<<< HEAD
     
-    <img
-     src="https://cf.bstatic.com/xdata/images/hotel/square600/51701821.webp?k=e0c37c19821bee729733b0926da9ec15a926c0ddcca84643a5da8036a5f335c7&o=" 
-     alt=""
-     className="fpImg"
-      />
-    <span className="fpName"> Aparthotel Tell Aviv</span>
-    <span className="fpCity"> Tell Aviv</span>
-=======
-    </div>
-    <div className="fp">
-    <img src="https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/https://www.cfmedia.vfmleonardo.com/imageRepo/7/0/106/179/502/L_club_garden_room_1_300_dpi_O/Leonardo-Club-Hotel-Eilat-Room.jpg?tr=w-780%2Ch-437%2Cfo-auto" alt=""className="fpImg" />0
+    <img src="https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/https://www.cfmedia.vfmleonardo.com/imageRepo/7/0/106/179/502/L_club_garden_room_1_300_dpi_O/Leonardo-Club-Hotel-Eilat-Room.jpg?tr=w-780%2Ch-437%2Cfo-auto" alt=""className="fpImg" />
     <span className="fpName"> Aparthotel Leonardo Hotel Jerusalem </span>
     <span className="fpCity"> Israel</span>
->>>>>>> 95dc31c02114263f054b90ae51b000c38ff372e7
     <span className="fpPrice"> Starting from $120</span>
     <div className="fpRating">
     <button>8.9</button>
     <span>Excellent</span>
-   </div>
-   <img
-     src="https://cf.bstatic.com/xdata/images/hotel/square600/217420129.webp?k=afc3bad17f4fbe2da918d293ce9c568c8d1485940fc9bfb3d92a7327d3b31f39&o=" 
-     alt=""
-     className="fpImg"
-      />
-    <span className="fpName"> Aparthotel Tell Aviv</span>
-    <span className="fpCity"> Tell Aviv</span>
-    <span className="fpPrice"> Starting from $120</span>
-    <div className="fpRating">
-    <button>8.9</button>
-    <span>Excellent</span>
-   </div>
-   <img
-     src="https://cf.bstatic.com/xdata/images/hotel/square600/368506092.webp?k=aae6169ebf02dfb29b9e2d679fda4d648687715f953e758b24a0a465ccdeb1d3&o=" 
-     alt=""
-     className="fpImg"
-      />
-    <span className="fpName"> Aparthotel Tell Aviv</span>
-    <span className="fpCity"> Tell Aviv</span>
-    <span className="fpPrice"> Starting from $120</span>
-    <div className="fpRating">
-    <button>8.9</button>
-    <span>Excellent</span>
-   </div>
-  
-   <img
-     src="https://cf.bstatic.com/xdata/images/hotel/square600/423106713.webp?k=daf2038ba61903db8f9b92adbaecfde5e462172673fbf235963a36b131eb86e5&o=" 
-     alt=""
-     className="fpImg"
-      />
-    <span className="fpName"> Aparthotel Tell Aviv</span>
-    <span className="fpCity"> Tell Aviv</span>
-    <span className="fpPrice"> Starting from $120</span>
-    <div className="fpRating">
-    <button>8.9</button>
-    <span>Excellent</span>
-   </div>
-   </div>
-   </div>
+    </div>
    
-<<<<<<< HEAD
-=======
     <img src="https://static.leonardo-hotels.com/image/herods-palace-executive-room-1_4e9550a9687b0eeeef6947b7f4d7d0df_1200x801_mobile_3.jpg" alt=""className="fpImg" />
     <span className="fpName"> Aparthotel Mitzpe Romon</span>
     <span className="fpCity">  Israel</span>
@@ -100,8 +41,8 @@ const FeaturedPop = () => {
     <span>Excellent</span>
     </div>
     </div>
+    
     </div>
->>>>>>> 95dc31c02114263f054b90ae51b000c38ff372e7
   );
 };
 
